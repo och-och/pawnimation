4498790463c3bd4e2290a4c18405656f2c043fa5
och-och <och.hco.och.hco@gmail.com>
2024-05-27 08:27:47 +0300

    switch to 4 tabs indentation

--- src/index.ts
+++ src/index.ts
@@ -1,60 +1,60 @@
 export class Paw {
-  options: KeyframeAnimationOptions = {
-    duration: 300,
-  };
+	options: KeyframeAnimationOptions = {
+		duration: 300,
+	};
 
-  constructor(private element: HTMLElement) {}
+	constructor(private element: HTMLElement) {}
 
-  ease(easing: string = "ease") {
-    this.options.easing = easing;
-    return this;
-  }
+	ease(easing: string = "ease") {
+		this.options.easing = easing;
+		return this;
+	}
 
-  duration(duration: number) {
-    this.options.duration = duration;
-    return this;
-  }
+	duration(duration: number) {
+		this.options.duration = duration;
+		return this;
+	}
 
-  composite(operation: CompositeOperation = "accumulate") {
-    this.options.composite = operation;
-  }
+	composite(operation: CompositeOperation = "accumulate") {
+		this.options.composite = operation;
+	}
 
-  animate(...frames: Keyframe[]) {
-    return new RunningPaw(this.element.animate(frames, this.options), false);
-  }
+	animate(...frames: Keyframe[]) {
+		return new RunningPaw(this.element.animate(frames, this.options), false);
+	}
 
-  animateStay(...frames: Keyframe[]) {
-    this.options.fill = "forwards";
-    return new RunningPaw(this.element.animate(frames, this.options), true);
-  }
+	animateStay(...frames: Keyframe[]) {
+		this.options.fill = "forwards";
+		return new RunningPaw(this.element.animate(frames, this.options), true);
+	}
 }
 
 export class RunningPaw {
-  constructor(
-    private animation: globalThis.Animation,
-    stay: boolean,
-  ) {
-    if (stay) {
-      this.animation.finished.then((animation) => {
-        animation.commitStyles();
-        animation.cancel();
-      });
-    }
-  }
+	constructor(
+		private animation: globalThis.Animation,
+		stay: boolean,
+	) {
+		if (stay) {
+			this.animation.finished.then((animation) => {
+				animation.commitStyles();
+				animation.cancel();
+			});
+		}
+	}
 
-  get finished() {
-    return this.animation.finished;
-  }
+	get finished() {
+		return this.animation.finished;
+	}
 
-  finish() {
-    this.animation.finish();
-  }
+	finish() {
+		this.animation.finish();
+	}
 
-  cancel() {
-    this.animation.cancel();
-  }
+	cancel() {
+		this.animation.cancel();
+	}
 }
 
 export function paw(element: HTMLElement) {
-  return new Paw(element);
+	return new Paw(element);
 }
