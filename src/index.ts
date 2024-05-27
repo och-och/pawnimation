d8a5b790f16b015789a5aaf140ce11383d333d7b
och-och <och.hco.och.hco@gmail.com>
2024-05-27 08:31:52 +0300

    add prettier

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,31 @@
 export class Paw {
 	options: KeyframeAnimationOptions = {
 		duration: 300,
-	};
+	}
 
 	constructor(private element: HTMLElement) {}
 
 	ease(easing: string = "ease") {
-		this.options.easing = easing;
-		return this;
+		this.options.easing = easing
+		return this
 	}
 
 	duration(duration: number) {
-		this.options.duration = duration;
-		return this;
+		this.options.duration = duration
+		return this
 	}
 
 	composite(operation: CompositeOperation = "accumulate") {
-		this.options.composite = operation;
+		this.options.composite = operation
 	}
 
 	animate(...frames: Keyframe[]) {
-		return new RunningPaw(this.element.animate(frames, this.options), false);
+		return new RunningPaw(this.element.animate(frames, this.options), false)
 	}
 
 	animateStay(...frames: Keyframe[]) {
-		this.options.fill = "forwards";
-		return new RunningPaw(this.element.animate(frames, this.options), true);
+		this.options.fill = "forwards"
+		return new RunningPaw(this.element.animate(frames, this.options), true)
 	}
 }
 
@@ -36,25 +36,25 @@ export class RunningPaw {
 	) {
 		if (stay) {
 			this.animation.finished.then((animation) => {
-				animation.commitStyles();
-				animation.cancel();
-			});
+				animation.commitStyles()
+				animation.cancel()
+			})
 		}
 	}
 
 	get finished() {
-		return this.animation.finished;
+		return this.animation.finished
 	}
 
 	finish() {
-		this.animation.finish();
+		this.animation.finish()
 	}
 
 	cancel() {
-		this.animation.cancel();
+		this.animation.cancel()
 	}
 }
 
 export function paw(element: HTMLElement) {
-	return new Paw(element);
+	return new Paw(element)
 }
